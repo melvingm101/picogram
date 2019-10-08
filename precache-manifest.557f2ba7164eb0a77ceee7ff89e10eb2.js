41f7617f3a8e6f67d751a793c0cb672ac403224c
Melvin <melvingm101@gmail.com>
2019-10-08 13:03:06 +0400

    Updates

---
+++ precache-manifest.557f2ba7164eb0a77ceee7ff89e10eb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b05b950aada16feae6f5cee647f2d6b4",
+    "url": "/picogram/index.html"
+  },
+  {
+    "revision": "96ae9642773c5f0cf82c",
+    "url": "/picogram/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "dbdb0498cc5dde7de003",
+    "url": "/picogram/static/css/main.56a6eef8.chunk.css"
+  },
+  {
+    "revision": "96ae9642773c5f0cf82c",
+    "url": "/picogram/static/js/2.6029c836.chunk.js"
+  },
+  {
+    "revision": "dbdb0498cc5dde7de003",
+    "url": "/picogram/static/js/main.2e4270c5.chunk.js"
+  },
+  {
+    "revision": "49bcad2ffc5d179d9528",
+    "url": "/picogram/static/js/runtime-main.52ddc2c4.js"
+  }
+]);
